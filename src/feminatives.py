2155ff6065fb6afacf80a8eb229dbb060caaa4a3
MoscowskyAnton <moscowskyad@gmail.com>
2020-07-13 01:50:22 +0300

    need try with much epoch

--- src/feminatives.py
+++ src/feminatives.py
@@ -27,7 +27,7 @@ def get_chars(words, characters, add_startend = False):
 def main():
     num_samples = -1
     batch_size = 32
-    epochs = 10
+    epochs = 2
     latent_dim = 265 # units in LSTM
     validation_split = 0.01
     data_train = pd.read_csv('../data_professions/professions_train_agumented.csv')
@@ -39,7 +39,7 @@ def main():
 
     # Vectorize the data
     characters = set()    
-    characters.add('\0')
+    characters.add(' ')
     input_words, characters = get_chars(input_words, characters)
     # adding a 'start char'    
     target_words, characters = get_chars(target_words, characters, True)
@@ -65,42 +65,51 @@ def main():
     
     for i, (input_word,target_word) in enumerate(zip(input_words,target_words)):
         for t, char in enumerate(input_word):
-            encoder_input_data[i, t, token_index[char]] = 1.        
-        for t in range(len(input_word),encoder_seq_length):
-            encoder_input_data[i, t, token_index['\0']] = 1.        
+            encoder_input_data[i, t, token_index[char]] = 1       
+        #for t in range(len(input_word),encoder_seq_length):
+            #encoder_input_data[i, t, token_index['\0']] = 1
+        encoder_input_data[i, t+1:, token_index[' ']] = 1    
         for t, char in enumerate(target_word):
-            decoder_input_data[i, t, token_index[char]] = 1.
+            decoder_input_data[i, t, token_index[char]] = 1
             if t > 0:
                 #decoder target data will be ahead by one timestamp (idk why) and will not include the 'start char'
-                decoder_target_data[i, t-1, token_index[char]] = 1.    
+                decoder_target_data[i, t-1, token_index[char]] = 1
         
-        for t in range(len(target_word),decoder_seq_length):
-            decoder_input_data[i, t, token_index['\0']] = 1.
-            decoder_target_data[i, t-1, token_index['\0']] = 1.         
+        #for t in range(len(target_word),decoder_seq_length):
+            #decoder_input_data[i, t, token_index['\0']] = 1
+            #decoder_target_data[i, t-1, token_index['\0']] = 1
+        decoder_input_data[i, t+1:, token_index[' ']] = 1
+        decoder_target_data[i, t:, token_index[' ']] = 1       
     
     for i, test_word in enumerate(test_words):
         for t, char in enumerate(test_word):
-            encoder_input_data_test[i, t, token_index[char]] = 1.
+            encoder_input_data_test[i, t, token_index[char]] = 1
         
-        for t in range(len(test_word),encoder_seq_length):
-            encoder_input_data_test[i, t, token_index['\0']] = 1.        
+        #for t in range(len(test_word),encoder_seq_length):
+            #encoder_input_data_test[i, t, token_index['\0']] = 1
+        encoder_input_data_test[i, t+1:, token_index[' ']] = 1     
         
     
     # define the input ans process
     encoder_inputs = Input(shape=(None, num_tokens))    
-    encoder1 = Bidirectional(LSTM(latent_dim,return_sequences=True))
-    encoder = Bidirectional(LSTM(latent_dim, return_state=True))
-    encoder_outputs, state_h_f, state_c_f, state_h_b, state_c_b = encoder(Dropout(0.25)(encoder1(encoder_inputs)))
+    #encoder1 = Bidirectional(LSTM(latent_dim,return_sequences=True))
+    #encoder = Bidirectional(LSTM(latent_dim, return_state=True))
+    encoder1 = LSTM(latent_dim,return_sequences=True)
+    encoder = LSTM(latent_dim, return_state=True)
+    #encoder_outputs, state_h_f, state_c_f, state_h_b, state_c_b = encoder(Dropout(0.25)(encoder1(encoder_inputs)))
+    encoder_outputs, state_h, state_c = encoder(Dropout(0.25)(encoder1(encoder_inputs)))
     # discard output, leave state only
-    state_h = Concatenate()([state_h_f, state_h_b])
-    state_c = Concatenate()([state_c_f, state_c_b])
-    encoder_states = [state_h, state_c]
+    #state_h = Concatenate()([state_h_f, state_h_b])
+    #state_c = Concatenate()([state_c_f, state_c_b])
+    encoder_states = [state_h, state_c]    
     
     # decoder
     decoder_inputs = Input(shape=(None,num_tokens))
     
-    decoder_lstm1 = LSTM(latent_dim * 2, return_sequences=True, return_state=True)
-    decoder_lstm = LSTM(latent_dim * 2, return_sequences=True, return_state=True)
+    #decoder_lstm1 = LSTM(latent_dim * 2, return_sequences=True, return_state=True)
+    #decoder_lstm = LSTM(latent_dim * 2, return_sequences=True, return_state=True)
+    decoder_lstm1 = LSTM(latent_dim, return_sequences=True, return_state=True)
+    decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
     decoder_outputs, _, _ = decoder_lstm(decoder_lstm1(decoder_inputs, initial_state=encoder_states))
     decoder_dense = Dense(num_tokens, activation='softmax')
     decoder_outputs = decoder_dense(decoder_outputs)
@@ -148,8 +157,10 @@ def main():
     encoder_model = Model(encoder_inputs, encoder_states)
     plot_model(encoder_model,to_file='../model_images/encoder_model_graph.png')
     
-    decoder_state_input_h = Input(shape=(latent_dim*2,))
-    decoder_state_input_c = Input(shape=(latent_dim*2,))
+    #decoder_state_input_h = Input(shape=(latent_dim*2,))
+    #decoder_state_input_c = Input(shape=(latent_dim*2,))
+    decoder_state_input_h = Input(shape=(latent_dim,))
+    decoder_state_input_c = Input(shape=(latent_dim,))
     decoder_state_inputs = [decoder_state_input_h, decoder_state_input_c]
     #decoder_outputs, state_h_f, state_c_f, state_h_b, state_c_b = decoder_lstm(decoder_inputs, initial_state = decoder_state_inputs)
     decoder_outputs, state_h, state_c = decoder_lstm(decoder_lstm1(decoder_inputs, initial_state = decoder_state_inputs))
@@ -167,12 +178,14 @@ def main():
         states_value = encoder_model.predict(input_word)
         
         target_seq = np.zeros((1,1,num_tokens))
-        target_seq[0,0,token_index['\t']] = 1.
+        target_seq[0,0,token_index['\t']] = 1
         
         stop_condition = False
         decoded_word = ''
         while not stop_condition:
             output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
+            
+            print(output_tokens)
             
             sampled_token_index = np.argmax(output_tokens[0, -1, :])
             sampled_char = reverse_char_index[sampled_token_index]
@@ -182,7 +195,7 @@ def main():
                 stop_condition = True
                 
             target_seq = np.zeros((1,1, num_tokens))
-            target_seq[0,0,sampled_token_index] = 1.
+            target_seq[0,0,sampled_token_index] = 1
             
             states_value = [h, c]
         
@@ -191,20 +204,22 @@ def main():
     print("++ Part of train set ++")
     for ind in range(int(len(input_words)*(1-2*validation_split)),int(len(input_words)*(1-validation_split))):
         input_seq = encoder_input_data[ind:ind+1]
-        decoded_word =decode_word(input_seq)                    
+        decoded_word =decode_word(input_seq)         
+        print("decoder target")
+        print(decoder_target_data[ind:ind+1,:,:])
         print("{} -> {}".format(input_words[ind],decoded_word))    
     
-    print("++ Validation set ++")
-    for ind in range(int(len(input_words)*(1-validation_split)),len(input_words)):
-        input_seq = encoder_input_data[ind:ind+1]
-        decoded_word =decode_word(input_seq)                    
-        print("{} -> {}".format(input_words[ind],decoded_word))
+    #print("++ Validation set ++")
+    #for ind in range(int(len(input_words)*(1-validation_split)),len(input_words)):
+        #input_seq = encoder_input_data[ind:ind+1]
+        #decoded_word =decode_word(input_seq)                    
+        #print("{} -> {}".format(input_words[ind],decoded_word))
     
-    print("++++++++ Test set +++++++++")
-    for ind in range(len(test_words)):
-        input_seq = encoder_input_data_test[ind:ind+1]
-        decoded_word =decode_word(input_seq)                    
-        print("{} -> {}".format(test_words[ind],decoded_word))
+    #print("++++++++ Test set +++++++++")
+    #for ind in range(len(test_words)):
+        #input_seq = encoder_input_data_test[ind:ind+1]
+        #decoded_word =decode_word(input_seq)                    
+        #print("{} -> {}".format(test_words[ind],decoded_word))
     
 if __name__ == '__main__':
     main()
