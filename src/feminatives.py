f534e3df570fd1c15d22a22cfee9814a48128dcd
MoscowskyAnton <moscowskyad@gmail.com>
2020-07-14 10:20:49 +0300

    not working

--- src/feminatives.py
+++ src/feminatives.py
@@ -27,8 +27,8 @@ def get_chars(words, characters, add_startend = False):
 def main():
     num_samples = -1
     batch_size = 32
-    epochs = 2
-    latent_dim = 265 # units in LSTM
+    epochs = 100
+    latent_dim = 512 #265 # units in LSTM
     validation_split = 0.01
     data_train = pd.read_csv('../data_professions/professions_train_agumented.csv')
     data_test = pd.read_csv('../data_professions/professions_train.csv')
